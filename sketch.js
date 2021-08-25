62b30f51d25e52839c3e44d7cf98a4caa60fed6f
SuperSid25 <86875156+SuperSid25@users.noreply.github.com>
2021-08-25 11:31:29 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+var sleep, brush, bg, gym, eat, drink, move, astronaut;
+
+
+function preload(){
+  bg = loadImage("images/iss.png");
+  sleep = loadAnimation("images/sleep.png");
+  brush = loadAnimation("images/brush.png");
+  gym = loadAnimation("images/gym21.png","gym22.png");
+  eat = loadAnimation("images/eat1.png","eat2.png");
+  drink = loadAnimation("images/drink1.png","drink2.png");
+  move = loadAnimation("images/move.png");
+  bath = loadAnimation("images/bath1.png","bath2.png");
+
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+  astronaut = createSprite(300,230);
+  astronaut.addAnimation("sleeping", sleep);
+  astronaut.scale = 0.1;
+}
+
+function draw() {
+  background(220);
+  image(bg,0,0,width,height);
+
+  if(keyDown("UP_ARROW")){
+    astronaut.addAnimation("brushing", brush);
+    astronaut.changeAnimation("brushing");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+    }
+
+    if(keyDown("DOWN_ARROW")){
+      astronaut.addAnimation("gymming", gym);
+      astronaut.changeAnimation("gymming");
+      astronaut.y = 350;
+      astronaut.velocityX = 0;
+      astronaut.velocityY = 0;
+      }
+
+      
+    if(keyDown("LEFT_ARROW")){
+      astronaut.addAnimation("eating", eat);
+      astronaut.changeAnimation("eating");
+      astronaut.y = 350;
+      astronaut.velocityX = 0;
+      astronaut.velocityY = 0;
+      }
+
+      if(keyDown("RIGHT_ARROW")){
+        astronaut.addAnimation("bathing", bath);
+        astronaut.changeAnimation("bathing");
+        astronaut.y = 350;
+        astronaut.velocityX = 0;
+        astronaut.velocityY = 0;
+        }
+
+      if(keyDown("m")){
+        astronaut.addAnimation("moving", move);
+        astronaut.changeAnimation("moving");
+        astronaut.y = 350;
+        astronaut.velocityX = 2;
+        astronaut.velocityY = 3;
+        }
+    
+    
+  
+  
+
+
+
+
+  drawSprites();
+  textSize(20);
+  fill(255);
+  text("To brush: press up arrow. To use the gym: press down arrow. To eat: press left arrow. To bath: press right arrow. To move: press m button.",300,30);
+}
